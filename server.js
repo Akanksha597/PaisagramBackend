27ad604f4b50967a6ac7c8b721d5bc8ad28451c2
Sam11dot <samruddhichaudhari185@gmail.com>
2026-01-09 14:45:00 +0530

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const { default: mongoose } = require("mongoose");
 
 dotenv.config();
 connectDB();
@@ -10,12 +11,34 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+let isconnected = false;
+async function connectDB(){
+  try{
+    await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true,
+  });  isconnected = true;
+    console.log("MongoDB connected");
+  }  catch(err){
+    console.error(err.message);
+    process.exit(1);
+  }
+}
 
+//add middleware to check db connection
+app.use((req, res, next) => {
+  if (!isConnected) {
+       connectDB(); // assuming connectDB is async
+  }
+    next();
+});
+  
 app.use("/api/campaion", require("./routes/CampaionRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/events", require("./routes/eventRoutes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on port ${PORT}`)
-);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () =>
+//   console.log(`🚀 Server running on port ${PORT}`)
+// );
+module.exports = app;
